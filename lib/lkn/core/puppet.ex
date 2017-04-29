343b3497939870e5c8bba0ac019bdd7a2a18e4b5
lthms <contact@thomasletan.fr>
2017-04-29 17:46:25 +0000

    entity: Simplify the implementation of both Puppet and Map

--- lib/lkn/core/puppet.ex
+++ lib/lkn/core/puppet.ex
@@ -3,37 +3,27 @@ defmodule Lkn.Core.Puppet do
   A key to identify and reach a Puppet.
   """
   @type k :: any
-  defp parse_def(lines, comps, legit) do
-    case lines do
-      [{:@, _, [{:components, _, [cps]}]}
-       |rest] ->
-        parse_def rest, cps, legit
-      [x|rest] ->
-        parse_def rest, comps, [x|legit]
-      [] ->
-        {comps, Enum.reverse(legit)}
-    end
-  end
-
   defmacro defpuppet(name, do: block) do
     lines = case block do
               {:__block__, _, x} -> x
               x -> [x]
             end
 
-    {comps, legit} = parse_def(lines, [], [])
-
     quote do
       defmodule unquote(name) do
         @after_compile __MODULE__
 
-        use Lkn.Core.Entity, components: unquote(comps)
+        unquote(lines)
 
-        unquote(legit)
+        def components do
+          @components
+        end
+
+        use Lkn.Core.Entity, components: @components
 
         def __after_compile__(_env, _bytecode) do
           # check if the components are effectively valid
-          Enum.map(unquote(comps), fn cx ->
+          Enum.map(components(), fn cx ->
             c = Macro.expand(cx, __MODULE__)
             gold = c.specs().system().component(:puppet)
 
